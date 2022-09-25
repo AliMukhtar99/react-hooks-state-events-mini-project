2458695a87cf04e36aea978aa553627d7236d4e9
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-09-25 14:32:15 +0300

    competed lab

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
+function TaskList({ selectedCategory, TASKS }) {
+  const selectedCategoryTasks = TASKS.filter((task) => {
+    if (selectedCategory === "All") return true;
+    return task.category === selectedCategory;
+  });
+  const [displayedTasks, setDisplayedTasks] = useState(selectedCategoryTasks);
 
-function TaskList() {
+  function handleDelete(text) {
+    setDisplayedTasks(() => {
+      return displayedTasks.filter((task) => task.text !== text);
+    });
+  }
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {displayedTasks.map((task, index) => {
+        return <Task key={index} task={task} handleDelete={handleDelete} />;
+      })}
     </div>
   );
 }
